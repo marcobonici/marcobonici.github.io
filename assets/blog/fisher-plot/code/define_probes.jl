095b07804466610bb6160d648d8b239d1eec0634
marcobonici <58727599+marcobonici@users.noreply.github.com>
2022-08-07 23:43:28 +0000

    Deploying to gh-pages from  @ 55e6c0a3480c8224aabdd6c61392cae8919b5572 🚀

--- assets/blog/fisher-plot/code/define_probes.jl
+++ assets/blog/fisher-plot/code/define_probes.jl
@@ -1,5 +1,5 @@
 # This file was generated, do not modify it. # hide
 LaTeXArray = [L"w_0", L"w_a", L"M_\nu\,[\mathrm{eV}]"]
-probes = [L"\mathrm{WL}", L"\mathrm{GC}",
+probes = [L"\mathrm{WL}", L"\mathrm{GC}_\mathrm{ph}",
 L"\mathrm{WL}\,+\,\mathrm{GC}_\mathrm{ph}\,+\,\mathrm{XC}"]
 colors = ["deepskyblue3", "darkorange1", "green",]
